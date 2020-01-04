7093a566411ace41bdc97891b38e7187e938ab5e
andrelp <andre.leon.poncelet@gmail.com>
2020-01-04 23:41:40 +0100

    added documentation for library

--- example/main.dart
+++ example/main.dart
@@ -2,20 +2,10 @@ import 'package:canny_edge_detection/canny_edge_detection.dart';
 import 'dart:io';
 import 'package:image/image.dart';
 
-void Function(Image image) safeImageCallBack(String outname) {
-  return (image) => File(outname).writeAsBytesSync(encodePng(image));;
-}
 
 void main() {
-  Image image = decodeImage(File("test_input.png").readAsBytesSync());
-  canny(
-    image,
-    blurRadius: 2,
-    onGrayConvertion: safeImageCallBack("test_outputA.png"),
-    onBlur: safeImageCallBack("test_outputB.png"),
-    onSobel: safeImageCallBack("test_outputC.png"),
-    onNonMaxSuppressed: safeImageCallBack("test_outputD.png")
-  );
-  File("test_outputEndResult.png").writeAsBytesSync(encodePng(image));
+  Image image = decodeImage(File("input.png").readAsBytesSync());
+  canny(image);
+  File("output.png").writeAsBytesSync(encodePng(image));
 }
 
--- lib/canny_edge_detection.dart
+++ lib/canny_edge_detection.dart
@@ -1,8 +1,9 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// This library exposed a function canny, which applies 
+/// cannys edge detection algorithm on images.
+/// For image inputs, this library used the image package
+/// which intern does not rely on dart:io, so it can be
+/// used for server, web, console or flutter!
 library canny_edge_detection;
 
 export 'src/canny.dart';
 
-// TODO: Export any libraries intended for clients of this package.
